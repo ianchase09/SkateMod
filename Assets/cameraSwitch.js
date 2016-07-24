d0a5a782f6adf3d29fc195b65ef7a83fb981461d
Ian Chase <ianchase09@gmail.com>
2016-07-24 16:56:17 -0400

    intial commit

---
+++ Assets/cameraSwitch.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var Camera1: GameObject;
+var Camera2: GameObject;
+var playerMotor: GameObject;
+
+
+function Start () {
+Camera1.active=true;
+Camera2.active=false;
+
+
+}
+
+function Update () { if (Input.GetKeyDown(KeyCode.F)) { Camera1.active = !Camera1.active; Camera2.active = !Camera2.active; } }
