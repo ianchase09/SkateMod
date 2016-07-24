2153154a8b027d39bca5f9371a4911d4ef81720f
Ian Chase <ianchase09@gmail.com>
2016-07-24 17:50:55 -0400

    implementation of cross platform input manager package

---
+++ Assets/objectSelect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class objectSelect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (CrossPlatformInputManager.GetAxis ("Horizontal"));	
+	}
+}
